c2ffb3bfdf0abcf192c5d5224fa03ef38b05c0b9
alexchwoj <chowiaudios@gmail.com>
2023-02-18 01:36:18 -0300

    Fixes

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 model = keras.models.load_model('pizza_model.h5')
 
-img = image.load_img('pizza.jpg', target_size=(224, 224))
+img = image.load_img('tarta.jpg', target_size=(224, 224))
 
 x = image.img_to_array(img)
 x = tf.keras.applications.mobilenet_v2.preprocess_input(x)
